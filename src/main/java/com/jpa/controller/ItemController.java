799d2339c5afc1b7fbf231fb2c29afe8021d990d
limseongkyu <96x60812@gmail.com>
2023-04-16 19:50:07 +0900

    Feat: ItemController

---
+++ src/main/java/com/jpa/controller/ItemController.java
@@ -0,0 +1,27 @@
+package com.jpa.controller;
+
+import com.jpa.domain.item.Book;
+import com.jpa.service.ItemService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ItemController {
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping(value = "/items/new")
+    public String createForm() {
+        return "items/createItemForm";
+    }
+
+    @PostMapping(value = "/items/new")
+    public String create(Book item) {
+        itemService.saveItem(item);
+        return "redirect:/items";
+    }
+}
